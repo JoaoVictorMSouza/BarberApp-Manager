c834d9b061e878073a8b51e8fce0cce2f383ee08
João Victor <63350577+JoaoVictorMSouza@users.noreply.github.com>
2023-09-27 12:50:29 -0300

    corrigindo warnings (#5)

--- BarberApp_Manager.Entity/Cabeleireiro/CabeleireiroEntity.cs
+++ BarberApp_Manager.Entity/Cabeleireiro/CabeleireiroEntity.cs
@@ -9,7 +9,7 @@
         public bool IsAceitandoServico { get; set; }
         public DateTime? LimiteServicoInicio { get; set; }
         public DateTime? LimiteServicoFim { get; set; }
-        public string Cpf { get; set; }
-        public string Email { get; set; }
+        public string? Cpf { get; set; }
+        public string? Email { get; set; }
     }
 }
